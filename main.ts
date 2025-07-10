846432251ff73a0d8c9ab017d939ef3448b12ac0
Johan Falk <johan@vaxjonexus.com>
2025-07-10 10:37:55 +0200

    Skapade funktioner för att blinka

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
 input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-	
+    blinkaR()
 })
+function blinkaL () {
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.red1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.green1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.blue1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.cyan)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.purple)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.white)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.yellow)
+}
+function blinkaR () {
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.red1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.green1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.blue1)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.cyan)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.purple)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.white)
+    MiniCar.led_rgb(LED_rgb_L_R.LED_R, LED_color.yellow)
+}
 basic.forever(function () {
 	
 })
